99c1d1ab2d48bfd336b49cc9a4e178807d7acdc4
juenv <juenvalj@gmail.com>
2020-01-27 10:25:58 -0800

    module 2

--- src/app.js
+++ src/app.js
@@ -8,9 +8,29 @@ app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
 
+const accountData = fs.readFileSync(path.join(__dirname, "json/accounts.json"), {encoding: 'UTF8'});
+const accounts = JSON.parse(accountData);
+const userData = fs.readFileSync(path.join(__dirname, "json/users.json"), {encoding: 'UTF8'});
+const users = JSON.parse(userData);
 
 app.get('/', function(req, resp) {
-    resp.render('index', {title: 'Index'});
+    resp.render('index', {title: 'Account Summary', accounts: accounts});
+});
+
+app.get('/savings', function(req, resp) {
+    resp.render('account', {account: accounts.savings});
+});
+
+app.get('/checking', function(req, resp) {
+    resp.render('account', {account: accounts.checking});
+});
+
+app.get('/credit', function(req, resp) {
+    resp.render('account', {account: accounts.credit});
+});
+
+app.get('/profile', function(req, resp) {
+    resp.render('profile', {user: users[0]});
 });
 
 app.listen(3000, () => console.log("PS Project Running on port 3000!"));
